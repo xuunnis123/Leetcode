f8932b64bd41dbe84f7017e4ebd5fa5b319a5eda
Ji-Yang Lin <xuunnis123@gmail.com>
2021-03-27 11:52:11 +0800

    Create 202.happy-number.cpp

---
+++ 202.happy-number.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=202 lang=cpp
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isHappy(int n) {
+       bool result=false;
+       int buffer=n;
+       int remain=1;
+       int thisTime=n;
+       if(n==1)return true;
+       do{
+           thisTime=calSum(thisTime);
+       }while(thisTime/10>0);
+       if(thisTime==1 || thisTime==7) return true;
+       else return thisTime==1;
+    }
+    int calSum(int n){
+        int save=0;
+        int buffer=n;
+        int remain=1;
+        while(remain>0){
+        remain=n/10;
+        buffer=n%10;
+        save+=buffer*buffer;
+        if(remain==0)return save;
+        n/=10;
+       }
+        return save;
+    }
+
+};
+// @lc code=end
+
